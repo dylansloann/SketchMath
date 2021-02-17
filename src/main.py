2d1763a5c21c95977e99bc0c2600653b379a82d6
Dylan Sloan <dylansloann2@gmail.com>
2021-02-17 13:39:52 -0600

    updated folder name

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from GUI import *
+
+# driver code
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
